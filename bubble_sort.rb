d5b64c8e1f67a7f30e4c0201ddaa6c0a9f547540
Joel Shapiro <joeladamshapiro@gmail.com>
2022-09-18 11:41:38 -0400

    Sort one by one in bubble format

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,20 @@
+def bubble_sort(list)
+    return list if list.size <= 1 #already sorted
+ 
+    loop do
+        swapped = false
+        0.upto(list.size-2) do |i|
+            if list[i] > list[i+1]
+                list[i], list[i+1] = list[i+1], list[i]
+                swapped = true
+            end
+        end
+        break unless swapped
+    end
+    p list
+end
 
+
+
+bubble_sort([4,3,78,2,0,2])
+# should return [0,2,2,3,4,78]
